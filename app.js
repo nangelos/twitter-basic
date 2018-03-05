ba724acfb1fabc828aabfaac4a63d4fac2256c85
nangelos <nicksangelos@gmail.com>
2018-03-05 17:10:12 -0600

    not logging verb yet

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require( 'express' );
 const app = express();
+const chalk = require('chalk');
 
 app.get('/', (req, res, next) => {
   res.send('Welcome')
-  next();
+  next()
 })
 
+app.get('/news', (req, res, next) => {
+  res.send('News')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log(req.verb, req.route);
+  next()
+})
 
 app.listen(3000,
 console.log('Listening on Port 3000')
