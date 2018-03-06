13f75e93078b94f2b495ee3c54f659f5aabdc369
nangelos <nicksangelos@gmail.com>
2018-03-06 14:49:48 -0600

    use morgan

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require( 'express' );
 const app = express();
-const chalk = require('chalk');
+// const chalk = require('chalk');
+const morgan = require('morgan');
+
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
   res.send('Welcome')
@@ -8,18 +12,10 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/news', (req, res, next) => {
-  res.send('News')
-  next()
+  res.json({name: 'newsRoute', data: 12345});
+  next();
 })
 
-app.use((req, res, next) => {
-  // console.log(req.route.stack[0].method.toUpperCase(), req.route.path);
-  process.stdout.write(chalk.yellow(req.route.stack[0].method.toUpperCase()) + ' ' + chalk.cyan(req.route.path + '\n'));
-  // console.log(res.req.rawHeaders.method);
-  // console.log(res.req.rawHeaders);
-  process.stdout.write(res.statusCode.toString() + '\n');
-  next()
-})
 
 app.listen(3000,
 console.log('Listening on Port 3000')
