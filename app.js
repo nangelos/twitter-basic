3706ca0cd234e3cdad29c31b16fc43950e635857
nangelos <nicksangelos@gmail.com>
2018-03-06 15:36:38 -0600

    lots of changes

--- app.js
+++ app.js
@@ -3,25 +3,22 @@ const app = express();
 // const chalk = require('chalk');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
 
 nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-
+const routes = require('./routes');
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res, next) => {
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-  next();
+app.use(function(req, res, next){
+  var mimeType = mime.lookup(req.path)
 })
 
-app.get('/news', (req, res, next) => {
-  res.json({name: 'newsRoute', data: 12345});
-  next();
-})
-
+app.use(routes);
 
 app.listen(3000,
 console.log('Listening on Port 3000')
