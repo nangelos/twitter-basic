fc6ed0b400d6cbdcbd7fd3351d8beb5fd4306ad4
nangelos <nicksangelos@gmail.com>
2018-03-06 15:09:59 -0600

    nunjucks almost working

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require( 'express' );
 const app = express();
 // const chalk = require('chalk');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 
 app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
-  res.send('Welcome')
-  next()
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+  next();
 })
 
 app.get('/news', (req, res, next) => {
@@ -20,3 +26,17 @@ app.get('/news', (req, res, next) => {
 app.listen(3000,
 console.log('Listening on Port 3000')
 );
+
+// var locals = {
+//   title: 'An Example',
+//   people: [
+//       { name: 'Gandalf'},
+//       { name: 'Frodo' },
+//       { name: 'Hermione'}
+//   ]
+// };
+
+// nunjucks.render('index.html', locals, function (err, output) {
+//   if (err) return console.error(err);
+//   console.log(output);
+// });
